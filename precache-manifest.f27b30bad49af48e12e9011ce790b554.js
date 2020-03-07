f320c03b31d6bd1392f1b8a9028f6c2f010205d1
1used23 <diziwii@gmail.com>
2020-03-08 01:47:15 +0300

    Updates

---
+++ precache-manifest.f27b30bad49af48e12e9011ce790b554.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ed6b9c7dc481108cdf7b3c3df2d2210",
+    "url": "/socket-chat/index.html"
+  },
+  {
+    "revision": "3a6cb877e75d735e2357",
+    "url": "/socket-chat/static/css/2.563af9af.chunk.css"
+  },
+  {
+    "revision": "7d79f6330fec2108884e",
+    "url": "/socket-chat/static/css/main.2420a8b8.chunk.css"
+  },
+  {
+    "revision": "3a6cb877e75d735e2357",
+    "url": "/socket-chat/static/js/2.9ff4a028.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/socket-chat/static/js/2.9ff4a028.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d79f6330fec2108884e",
+    "url": "/socket-chat/static/js/main.a0b9a2f8.chunk.js"
+  },
+  {
+    "revision": "6f01747b3e754a66f54f",
+    "url": "/socket-chat/static/js/runtime-main.40857fde.js"
+  }
+]);
